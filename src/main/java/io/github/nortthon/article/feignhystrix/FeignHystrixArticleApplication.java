2a30035c58f6e3390eeedb29693ca9dc57efba4c
Lucas Augusto <nortthon@gmail.com>
2020-02-29 03:02:50 -0300

    initial commit

---
+++ src/main/java/io/github/nortthon/article/feignhystrix/FeignHystrixArticleApplication.java
@@ -0,0 +1,13 @@
+package io.github.nortthon.article.feignhystrix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FeignHystrixArticleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FeignHystrixArticleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/nortthon/article/feignhystrix/FeignHystrixArticleApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.nortthon.article.feignhystrix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FeignHystrixArticleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
